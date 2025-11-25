3dcc8c3862e45079ec2bff2f2b81eb1546395dcf
Jeremy Harrington <harringtonj6@xavier.edu>
2025-11-24 23:49:07 -0500

    Add project files.

---
+++ ModMenu.ExampleMod/Example Mod.cs
@@ -0,0 +1,16 @@
+﻿using StarMap.API;
+using ModMenu;
+using Brutal.ImGuiApi;
+
+namespace ModMenu.ExampleMod
+{
+    [StarMapMod]
+    public class ExampleMod
+    {
+        [ModMenuEntry("ModMenu ExampleMod")]
+        public void DrawSubMenuEntry()
+        {
+            ImGui.Text("The test worked!");
+        }
+    }
+}
